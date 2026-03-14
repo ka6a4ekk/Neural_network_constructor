429f0403109a7935b5370214cbba3a03cf5fc394
ka6a4ekk <140659610+ka6a4ekk@users.noreply.github.com>
2026-03-15 01:06:58 +0200

    Delete build directory

--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/StencilMode.cpp.o: \
- /home/ivan/projects/NNC/build/_deps/sfml-src/src/SFML/Graphics/StencilMode.cpp \
- /usr/include/stdc-predef.h \
- /home/ivan/projects/NNC/build/_deps/sfml-src/include/SFML/Graphics/StencilMode.hpp \
- /home/ivan/projects/NNC/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- /home/ivan/projects/NNC/build/_deps/sfml-src/include/SFML/Config.hpp
